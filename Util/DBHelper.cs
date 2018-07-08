b718221e33efa5d0e423c783ee19deab04223c4a
zhaotianff <zhaotianff@163.com>
2018-07-08 11:56:27 +0800

    Add Utility Class

---
+++ Util/DBHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+namespace SQLiteTool.Util
+{
+    class DBHelper
+    {
+        private void CreateDB(string dbName, string DbFilePath)
+        {
+
+        }
+
+        private bool OpenLocalDB(string dbName)
+        {
+            try
+            {
+                string conStr = "server=.database=" + dbName;
+                SQLiteConnection con = new SQLiteConnection(conStr);
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+            return false;
+        }
+
+        private bool CloseLocalDB(string dbName)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ Util/GlobalData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SQLite;
+
+namespace SQLiteTool.Util
+{
+    class GlobalData
+    {
+        private static object obj = new object();
+        private static GlobalData _globalData;
+
+        DBHelper dbHelper;
+
+        public static GlobalData CreateInstance()
+        {
+            if(_globalData == null)
+            {
+                lock(obj)
+                {
+                    if (_globalData == null)
+                        _globalData = new GlobalData();
+                }
+            }
+            return _globalData;
+        }
+
+        public GlobalData()
+        {
+            dbHelper = new DBHelper();
+        }
+
+    }
+}
